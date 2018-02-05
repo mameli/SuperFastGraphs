a0c1b08945980f37917a281a928c1cb8b3bdcc31
iskorini <iskorini@gmail.com>
2018-02-05 22:07:01 +0100

    bugfix pruned bfs

--- src/SuperFastGraphs.jl
+++ src/SuperFastGraphs.jl
@@ -10,6 +10,7 @@ export triangleCounting
 export triangleCountingDegree
 export prunedBFS
 export preProcess
+export fastClosenessCentrality
 
 function sampleDistance(g::AbstractGraph, precision::Int64 )
 	numV = nv(g)
@@ -145,23 +146,23 @@ function triangleCountingDegree(g::AbstractGraph)
 	return triangleNumber
 end
 
-function prunedBFS(g::AbstractGraph, v::Int64, x::Int64, preProcessData )
+function prunedBFS(g::AbstractGraph, v::Int64, x::Float64, preProcessData )
 	q = Queue(Int64)
 	visited = zeros(Int64, nv(g)) #0: not visited, 1:visited, 2:completely explored
 	enqueue!(q, v)
 	f = Int64[]
 	push!(f, v)
-	depth = 0
+	depth = 1
 	timeToDepthIncrease = 1
 	elementsToDepthIncrease = 0
 	nodeAtDepth = Int64[]
 	fdValue = 0
 	nodeExplored = 0
 	ccV = 1
-	while !in(v, preProcessData[ccV]) #trovo la componente connessa di v
+	while !in(v, preProcessData[1][ccV]) #trovo la componente connessa di v
 		ccV = ccV + 1
 	end
-
+	println("---VERTICE ", v,": ")
 	while length(q)>0
 		e = dequeue!(q)
 		push!(nodeAtDepth, e)
@@ -170,18 +171,25 @@ function prunedBFS(g::AbstractGraph, v::Int64, x::Int64, preProcessData )
 		elementsToDepthIncrease = elementsToDepthIncrease + length(neighborhood)
 		timeToDepthIncrease = timeToDepthIncrease - 1
 		if timeToDepthIncrease == 0
-			#computing bound
 			n = length(vertices(g))
 			fdValue = fdValue + (length(nodeAtDepth)*depth)
+			println("fdValue: ",fdValue)
 			nodeExplored = nodeExplored + length(nodeAtDepth)
+			println("n_d: ", nodeExplored)
 			gamma_d1 = 0
 			for node in nodeAtDepth
-			    gamma_d1 = gamma_d1 + degree(node)
+				gamma_d1 = gamma_d1 + degree(g, node)
 			end
-			bound = max( ((preProcessData[2][ccV]-1)^2)/(fdValue - gamma_d1 + (depth +2)*( preProcessData[2][ccV]- nodeExplored)) ,
-						 ((preProcessData[3][ccV]-1)^2)/(fdValue - gamma_d1 + (depth +2)*( preProcessData[3][ccV]- nodeExplored))
-						) / (n-1)
+			println("gamma_d1: ",gamma_d1)
+			fdTildeValueAlpha = fdValue - gamma_d1 + (depth+2)*(preProcessData[2][ccV] - nodeExplored)
+			println("fdtilde: ",fdTildeValueAlpha)
+			fdTildeValueOmega = fdValue - gamma_d1 + (depth+2)*(preProcessData[2][ccV] - nodeExplored)
+			a = ((preProcessData[2][ccV]-1)^2) / fdTildeValueAlpha
+			b = ((preProcessData[3][ccV]-1)^2) / fdTildeValueOmega
+			bound = max( a, b ) / (n-1)
+			println("bound: ", bound)
 			if x > bound
+				println("v: ", v, " bound: ", bound, " NON TOPK")
 				return 0 #il nodo non è tra i topk
 			end
 			#
@@ -198,7 +206,13 @@ function prunedBFS(g::AbstractGraph, v::Int64, x::Int64, preProcessData )
 			end
 		end
 	end
-	return ((length(preProcessData[ccV]) -1 )^2)/((nv(g)-1)*fdValue)
+
+	println("RITORNO VALORE->")
+	println("fvalue: ", fdValue)
+	println("rv: ", preProcessData[1][ccV])
+	d = dijkstra_shortest_paths(g,v).dists
+    δ = filter(x->x != typemax(x), d)
+	return ((length(preProcessData[1][ccV]) - 1 )^2)/((nv(g)-1)*sum(δ))
 end
 
 function preProcess(g::AbstractGraph) #preprocess for compute upper boud of closeness centrality
@@ -241,17 +255,24 @@ function preProcess(g::AbstractGraph) #preprocess for compute upper boud of clos
 end
 
 function fastClosenessCentrality(g::AbstractGraph)
-    preprocessData = preProcess(g)
-    xk = 0
-    topk = Int64[]
-    for v in vertices(g)
-    	t_xk = prunedBFS(g, v, xk, preProcessData)
-    	if t_xk != 0
-    		xk = t_xk
-    		push!(topk, v)
-    	end
-    end
-	#TODO: order by degree
+	preprocessData = preProcess(g)
+	println("Dati PreProcess: ", preprocessData)
+	xk = 0.0
+	topk = []
+	for v in vertices(g)
+		
+		if (degree(g, v) != 0)
+			#t_xk = prunedBFS(g, v, xk, preprocessData)
+			t_xk = prunedBFS(g, v, 0.0, preprocessData)
+			println(v," --- analizzato --->", t_xk)
+			if t_xk != 0
+				xk = t_xk
+				push!(topk, (v, xk))
+				println(v," --- trovato top k --->", xk)
+			end
+		end
+	end
+	return topk
 end
 
 end # module
--- test/test_cc.jl
+++ test/test_cc.jl
@@ -2,26 +2,26 @@ using SuperFastGraphs
 using Base.Test
 using LightGraphs
 
-g = DiGraph(6)
-
-add_edge!(g, 1, 2)
-add_edge!(g, 1, 3)
-add_edge!(g, 1, 4)
-add_edge!(g, 5, 1)
-add_edge!(g, 2, 3)
-add_edge!(g, 3, 6)
-add_edge!(g, 4, 5)
-println(preProcess(g))
-
-println("-----------------")
-g = DiGraph(3)
-add_edge!(g, 1, 2)
-add_edge!(g, 2, 3)
-add_edge!(g, 3, 1)
-println(preProcess(g))
-
-println("---------------")
-
+#g = DiGraph(6)
+#
+#add_edge!(g, 1, 2)
+#add_edge!(g, 1, 3)
+#add_edge!(g, 1, 4)
+#add_edge!(g, 5, 1)
+#add_edge!(g, 2, 3)
+#add_edge!(g, 3, 6)
+#add_edge!(g, 4, 5)
+#println(preProcess(g))
+#
+#println("-----------------")
+#g = DiGraph(3)
+#add_edge!(g, 1, 2)
+#add_edge!(g, 2, 3)
+#add_edge!(g, 3, 1)
+#println(preProcess(g))
+#
+#println("---------------")
+#
 g = Graph(6)
 add_edge!(g, 1, 2)
 add_edge!(g, 1, 3)
@@ -31,3 +31,10 @@ add_edge!(g, 5, 4)
 println("connesso: ",is_connected(g))
 println("componenti connesse: ", connected_components(g))
 println(preProcess(g))
+
+println("-------------------")
+#g = loadgraph("testdata/karate.lg", "graph")
+println(fastClosenessCentrality(g))
+println("-----------------------")
+println(closeness_centrality(g))
+#println(connected_components(g))
