03624b5d415c987e8d4cf5d23b6a6b0ea105f5be
Filippo Mameli <mameli93@gmail.com>
2018-02-11 16:10:17 +0100

    test preprocess

--- test/runtests.jl
+++ test/runtests.jl
@@ -43,6 +43,31 @@ triangleNumberD = triangleCountingDegree(g)
 @test (triangleNumber == (sum(triangles(g))/3))
 @test (triangleNumberD == (sum(triangles(g))/3))
 
+## TEST PREPROCESS
+gDWeakly = DiGraph(6)
+add_edge!(gDWeakly, 1, 2)
+add_edge!(gDWeakly, 1, 3)
+add_edge!(gDWeakly, 1, 4)
+add_edge!(gDWeakly, 5, 1)
+add_edge!(gDWeakly, 2, 3)
+add_edge!(gDWeakly, 3, 6)
+add_edge!(gDWeakly, 4, 5)
+@test preProcess(gDWeakly) == ([[1, 4, 5], [2], [3], [6]], [6, 3, 2, 1], [10, 4, 2, 1])
 
+gDStrongly = DiGraph(3)
+add_edge!(gDStrongly, 1, 2)
+add_edge!(gDStrongly, 2, 3)
+add_edge!(gDStrongly, 3, 1)
+@test preProcess(gDStrongly) == ([[1, 2, 3]], [3], [3])
+
+gNotD_NotC = Graph(6)
+add_edge!(gNotD_NotC, 1, 2)
+add_edge!(gNotD_NotC, 1, 3)
+add_edge!(gNotD_NotC, 2, 4)
+add_edge!(gNotD_NotC, 2, 5)
+add_edge!(gNotD_NotC, 5, 4)
+@test preProcess(gNotD_NotC) == ([[1, 2, 3, 4, 5], [6]], [5, 1], [5, 1])
+
+## TEST FASTCLOSENESSCENTRALITY
 @test ((sort!(closeness_centrality(g)))[end] == fastClosenessCentrality(g, 1)[1][2])
 @test ((sort!(closeness_centrality(g)))[end - 5] == fastClosenessCentrality(g, 5)[1][2])
